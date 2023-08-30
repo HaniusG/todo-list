994a269e390448d6b1f8c298cb8825075dec5c42
Grigor Ohanyan <ohanyang183@gmail.com>
2023-08-30 22:23:11 +0400

    big css update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <div className='header'>
       <h1>My Todo List</h1>
-      <span>Done: 0</span>
-      <br />
-      <span>Important: 2</span>
+      <div><span>Done: 0</span>
+        <br />
+        <span>Important: 2</span></div>
     </div>
   );
 }
